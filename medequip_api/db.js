7973798c6b6d68f0d36d857ee2b6174533c8f452
AMADA05 <grarouisrael5@gmail.com>
2026-09-08 11:29:28 +0000

    Ajout de la configuration SSL dans db.js pour Neon

--- medequip_api/db.js
+++ medequip_api/db.js
@@ -6,7 +6,10 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false // Active la connexion SSL requise par Neon.tech
+  }
 });
 
 pool.on('connect', () => {
